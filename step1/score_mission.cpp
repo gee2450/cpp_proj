d9d24b150226712569f7243f39c09fb5f2ea3fa1
Minji Yun <gee2450@kookmin.ac.kr>
2021-06-17 01:37:38 +0900

    Update step1 src
    Add checking function

--- step1/score_mission.cpp
+++ step1/score_mission.cpp
@@ -11,9 +11,16 @@ class Score {
   int poison_items;
   int gate_cnt;
   WINDOW* win;
+  friend class Mission;
 public:
   Score(WINDOW& xwin);
   WINDOW& update();
+  void plus_current_length();
+  void minus_current_length();
+  void plus_max_length();
+  void plus_growth_items();
+  void plus_poison_items();
+  void plus_gate_cnt();
 };
 
 Score::Score(WINDOW& xwin) {
@@ -26,8 +33,10 @@ Score::Score(WINDOW& xwin) {
   box(win, 0, 0);
 }
 WINDOW& Score::update() {
+  string ret;
+
   mvwprintw(win, 1, 1, "Score Board");
-  string ret = "B: " + to_string(current_length) + "/" + to_string(max_length);
+  ret = "B: " + to_string(current_length) + "/" + to_string(max_length);
   mvwprintw(win, 2, 1, ret.c_str() );
   ret = "+: " + to_string(growth_items);
   mvwprintw(win, 3, 1, ret.c_str());
@@ -46,10 +55,15 @@ class Mission {
   int growth_items;
   int poison_items;
   int gate_cnt;
+  bool length_check;
+  bool growth_items_check;
+  bool poison_items_check;
+  bool gate_cnt_check;
   WINDOW* win;
 public:
   Mission(WINDOW&, int, int, int, int);
   WINDOW& update();
+  void check(Score& score);
 };
 
 Mission::Mission( WINDOW& xwin, int length, int growth_items,
@@ -58,21 +72,34 @@ Mission::Mission( WINDOW& xwin, int length, int growth_items,
   this->growth_items = growth_items;
   this->poison_items = poison_items;
   this->gate_cnt = gate_cnt;
+  length_check = false;
+  growth_items_check = false;
+  poison_items_check = false;
+  gate_cnt_check = false;
   win = &xwin;
   box(win, 0, 0);
 }
 WINDOW& Mission::update() {
+  string ret;
+
   mvwprintw(win, 1, 1, "Mission");
-  string ret = "B: " + to_string(length);
+  ret = "B: " + to_string(length) + " (" + ((length_check) ? "V)" : " )");
   mvwprintw(win, 2, 1, ret.c_str() );
-  ret = "+: " + to_string(growth_items);
+  ret = "+: " + to_string(growth_items) + " (" + ((growth_items_check) ? "V)" : " )");
   mvwprintw(win, 3, 1, ret.c_str());
-  ret = "-: " + to_string(poison_items);
+  ret = "-: " + to_string(poison_items) + " (" + ((poison_items_check) ? "V)" : " )");
   mvwprintw(win, 4, 1, ret.c_str());
-  ret = "G: " + to_string(gate_cnt);
+  ret = "G: " + to_string(gate_cnt) + " (" + ((gate_cnt_check) ? "V)" : " )");
   mvwprintw(win, 5, 1, ret.c_str());
   touchwin(win);
   wrefresh(win);
 
   return *win;
+}
+
+void Mission::check(Score& score) {
+  length_check = (score.max_length >= length) ? true : false;
+  growth_items_check = (score.growth_items >= growth_items) ? true : false;
+  poison_items_check = (score.poison_items <= poison_items) ? true : false;
+  gate_cnt_check = (score.gate_cnt >= gate_cnt) ? true : false;
 }
